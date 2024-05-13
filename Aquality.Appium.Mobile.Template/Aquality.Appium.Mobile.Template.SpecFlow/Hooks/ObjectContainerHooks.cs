c190fbeb3330222cea2f65ae308ca08b04b6b80a
Aliaksej Mialeshka <a2.meleshko@a1qa.com>
2024-05-13 19:00:56 +0200

    Migrate to .NET 8 and NUnit 4. Use new language features, such as primary constructor Update package references Add application termination

--- Aquality.Appium.Mobile.Template/Aquality.Appium.Mobile.Template.SpecFlow/Hooks/ObjectContainerHooks.cs
+++ Aquality.Appium.Mobile.Template/Aquality.Appium.Mobile.Template.SpecFlow/Hooks/ObjectContainerHooks.cs
@@ -1,5 +1,4 @@
 ﻿using Aquality.Appium.Mobile.Applications;
-using Aquality.Appium.Mobile.Screens.ScreenFactory;
 using Aquality.Appium.Mobile.Template.Utilities;
 using BoDi;
 using TechTalk.SpecFlow;
@@ -7,19 +6,13 @@ using TechTalk.SpecFlow;
 namespace Aquality.Appium.Mobile.Template.SpecFlow.Hooks
 {
     [Binding]
-    public class ObjectContainerHooks
+    public class ObjectContainerHooks(IObjectContainer objectContainer)
     {
-        private readonly IObjectContainer objectContainer;
-
-        public ObjectContainerHooks(IObjectContainer objectContainer)
-        {
-            this.objectContainer = objectContainer;
-        }
 
         [BeforeScenario]
         public void RegisterDependencies()
         {
-            objectContainer.RegisterInstanceAs<IScreenFactory>(AqualityServices.ScreenFactory);
+            objectContainer.RegisterInstanceAs(AqualityServices.ScreenFactory);
             objectContainer.RegisterInstanceAs<IScreenshotProvider>(new ScreenshotProvider());
         }
     }
